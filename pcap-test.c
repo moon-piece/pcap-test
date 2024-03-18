aec03ff344821a63a2ab4c785dfd631a8625169b
moon <moon_piece@naver.com>
2024-03-18 10:53:50 -0400

    Update pcap-test.c

--- pcap-test.c
+++ pcap-test.c
@@ -1,6 +1,118 @@
 #include <pcap.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <netinet/in.h> // ntohs
+#define ETHER_ADDR_LEN 6
+#define ETHERTYPE_IP 0x0800
+
+/*
+ *  Ethernet II header
+ *  Static header size: 14 bytes
+ */
+struct libnet_ethernet_hdr   // libnet-headers.h > struct libnet_ethernet_hdr
+{
+    u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
+    u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
+    u_int16_t ether_type;                 /* protocol */
+};
+
+/*
+ *  IPv4 header
+ *  Internet Protocol, version 4
+ *  Static header size: 20 bytes
+ */
+struct libnet_ipv4_hdr   // libnet-headers.h > struct libnet_ipv4_hdr
+{
+#if (LIBNET_LIL_ENDIAN)
+    u_int8_t ip_hl:4,      /* header length */
+        ip_v:4;         /* version */
+#endif
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t ip_v:4,       /* version */
+        ip_hl:4;        /* header length */
+#endif
+    u_int8_t ip_tos;       /* type of service */
+#ifndef IPTOS_LOWDELAY
+#define IPTOS_LOWDELAY      0x10
+#endif
+#ifndef IPTOS_THROUGHPUT
+#define IPTOS_THROUGHPUT    0x08
+#endif
+#ifndef IPTOS_RELIABILITY
+#define IPTOS_RELIABILITY   0x04
+#endif
+#ifndef IPTOS_LOWCOST
+#define IPTOS_LOWCOST       0x02
+#endif
+    u_int16_t ip_len;         /* total length */
+    u_int16_t ip_id;          /* identification */
+    u_int16_t ip_off;
+#ifndef IP_RF
+#define IP_RF 0x8000        /* reserved fragment flag */
+#endif
+#ifndef IP_DF
+#define IP_DF 0x4000        /* dont fragment flag */
+#endif
+#ifndef IP_MF
+#define IP_MF 0x2000        /* more fragments flag */
+#endif
+#ifndef IP_OFFMASK
+#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
+#endif
+    u_int8_t ip_ttl;          /* time to live */
+    u_int8_t ip_p;            /* protocol */
+    u_int16_t ip_sum;         /* checksum */
+    struct in_addr ip_src, ip_dst; /* source and dest address */
+};
+
+/*
+ *  TCP header
+ *  Transmission Control Protocol
+ *  Static header size: 20 bytes
+ */
+struct libnet_tcp_hdr  // libnet-headers.h > struct libnet_tcp_hdr
+{
+    u_int16_t th_sport;       /* source port */
+    u_int16_t th_dport;       /* destination port */
+    u_int32_t th_seq;          /* sequence number */
+    u_int32_t th_ack;          /* acknowledgement number */
+#if (LIBNET_LIL_ENDIAN)
+    u_int8_t th_x2:4,         /* (unused) */
+        th_off:4;        /* data offset */
+#endif
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t th_off:4,        /* data offset */
+        th_x2:4;         /* (unused) */
+#endif
+    u_int8_t  th_flags;       /* control flags */
+#ifndef TH_FIN
+#define TH_FIN    0x01      /* finished send data */
+#endif
+#ifndef TH_SYN
+#define TH_SYN    0x02      /* synchronize sequence numbers */
+#endif
+#ifndef TH_RST
+#define TH_RST    0x04      /* reset the connection */
+#endif
+#ifndef TH_PUSH
+#define TH_PUSH   0x08      /* push data to the app layer */
+#endif
+#ifndef TH_ACK
+#define TH_ACK    0x10      /* acknowledge */
+#endif
+#ifndef TH_URG
+#define TH_URG    0x20      /* urgent! */
+#endif
+#ifndef TH_ECE
+#define TH_ECE    0x40
+#endif
+#ifndef TH_CWR
+#define TH_CWR    0x80
+#endif
+    u_int16_t th_win;         /* window */
+    u_int16_t th_sum;         /* checksum */
+    u_int16_t th_urp;         /* urgent pointer */
+};
 
 void usage() {
 	printf("syntax: pcap-test <interface>\n");
@@ -45,6 +157,40 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 		printf("%u bytes captured\n", header->caplen);
+
+        // Ethernet Header - src mac / dst mac
+        struct libnet_ethernet_hdr *eth_hdr = packet;
+        if (ntohs(eth_hdr->ether_type) != ETHERTYPE_IP) continue;
+        printf("type = %04x \n", ntohs(eth_hdr->ether_type)); // IPv4 = 0800
+
+        printf("scr mac: ");
+        // src mac
+        for(int i=0; i<5; i++) printf("%02x:", eth_hdr->ether_shost[i]);
+        printf("%02x\n", eth_hdr->ether_shost[5]);
+        // dst mac
+        printf("dst mac: ");
+        for(int i=0; i<5; i++) printf("%02x:", eth_hdr->ether_dhost[i]);
+        printf("%02x\n", eth_hdr->ether_dhost[5]);
+
+
+        // IP Header -  src ip / dst ip
+        struct libnet_ipv4_hdr *ip_hdr = packet + sizeof(struct libnet_ethernet_hdr);
+        if (ip_hdr->ip_p != IPPROTO_TCP) continue;
+        printf("proto = %d \n", ip_hdr->ip_p); // IPPROTO_TCP = 6
+
+        // src ip
+        printf("src ip: ");
+        printf("%x\n", htonl(ip_hdr->ip_src.s_addr));
+
+        printf("dst ip: ");
+        printf("%x\n", htonl(ip_hdr->ip_dst.s_addr));
+
+
+        // TCP Header - src port / dst port
+        struct libnet_tcp_hdr *tcp_hdr = packet + sizeof(struct libnet_ethernet_hdr) + sizeof(struct libnet_ipv4_hdr);
+        // printf("%d", tcp_hdr->th_sport);
+
+        // Payload(Data) - hexadecimal value (Maximum : 10 Byte)
 	}
 
 	pcap_close(pcap);
